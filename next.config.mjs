28daecb3ec1357795b848e113a1c862e7f4df477
ciocio97 <ciociolee97@gmail.com>
2025-03-06 13:57:30 +0900

    feat : fix deploy css & js 404 error

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,14 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
-  // distDir: "./dist", // Changes the build output directory to `./dist/`.
+  output: "export", // SSG
+  distDir: "build",
+  trailingSlash: true, // GitHub Pages에서 index.html 찾을 수 있도록 설정
   env: {
     NEXT_PUBLIC_BASE_PATH: isProd ? "https://ciocio97.github.io" : "",
+  },
+  images: {
+    unoptimized: true,
   },
 };
 
